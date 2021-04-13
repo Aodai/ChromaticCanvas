18589d7649b81543d19b66c68a5d9b0f1701e64f
Aodai <odai@odaiq.co>
2021-04-13 20:10:52 +0300

    Created ChromaticCanvasDbContext Created BaseRepository implementing IRepository interface Created MemberRepository extending BaseRepository as an example for my colleagues

---
+++ ChromaticCanvas/ChromaticCanvas.DataAccess/ChromaticCanvasDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ChromaticCanvas.ApplicationLogic.DataModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChromaticCanvas.DataAccess
+{
+    public class ChromaticCanvasDbContext : DbContext
+    {
+        ChromaticCanvasDbContext(DbContextOptions<ChromaticCanvasDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ ChromaticCanvas/ChromaticCanvas.DataAccess/MemberRepository.cs
@@ -0,0 +1,21 @@
+﻿using ChromaticCanvas.ApplicationLogic.Abstractions;
+using ChromaticCanvas.ApplicationLogic.DataModel;
+using System;
+using System.Linq;
+
+namespace ChromaticCanvas.DataAccess
+{
+    public class MemberRepository : BaseRepository<Member>, IMemberRepository
+    {
+
+        public MemberRepository(ChromaticCanvasDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Member GetMemberById(Guid userID)
+        {
+            return dbContext.Members
+                .Where(member => member.ID == userID).SingleOrDefault();
+        }
+    }
+}
