c524652ce68d42543607d4790009aad55b4e9f59
bombecuhidrogen <80785059+bombecuhidrogen@users.noreply.github.com>
2021-04-14 17:13:05 +0300

    Forgot to save the file

--- ChromaticCanvas/ChromaticCanvas.DataAccess/BookingRepository.cs
+++ ChromaticCanvas/ChromaticCanvas.DataAccess/BookingRepository.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ChromaticCanvas.ApplicationLogic.Abstractions;
+using ChromaticCanvas.ApplicationLogic.DataModel;
+using System;
+using System.Linq;
 
 namespace ChromaticCanvas.DataAccess
 {
-    class BookingRepository
+    public class BookingRepository : BaseRepository<Booking>, IBookingRepository
     {
+        public BookingRepository(ChromaticCanvasDbContext dbContext) : base(dbContext)
+        { }
+        public Booking GetBookingById(Guid id)
+        {
+            return dbContext.Bookings
+                .Where(booking => booking.ID == id).SingleOrDefault();
+        }
     }
 }
