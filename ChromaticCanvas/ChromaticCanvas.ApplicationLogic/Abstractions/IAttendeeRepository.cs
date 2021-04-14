f49131af0d59c0075fdea1627b1577fa1585e07e
Aodai <odai@odaiq.co>
2021-04-14 21:07:33 +0300

    Implemented AttendeeRepository

---
+++ ChromaticCanvas/ChromaticCanvas.ApplicationLogic/Abstractions/IAttendeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using ChromaticCanvas.ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+
+namespace ChromaticCanvas.ApplicationLogic.Abstractions
+{
+    public interface IAttendeeRepository : IRepository<Attendee>
+    {
+        List<Attendee> GetAttendeesByEventId(Guid id);
+    }
+}
---
+++ ChromaticCanvas/ChromaticCanvas.DataAccess/AttendeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChromaticCanvas.ApplicationLogic.Abstractions;
+using ChromaticCanvas.ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChromaticCanvas.DataAccess
+{
+    public class AttendeeRepository : BaseRepository<Attendee>, IAttendeeRepository
+    {
+        public AttendeeRepository(ChromaticCanvasDbContext dbContext) : base(dbContext) { }
+
+        public List<Attendee> GetAttendeesByEventId(Guid id)
+        {
+            return dbContext.Attendees
+                .Where(eventt => eventt.Event.ID == id).ToList<Attendee>();
+        }
+    }
+}
