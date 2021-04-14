91d5b4cd8e0a2e2ff24e9358215e9c9b326b6add
Aodai <odai@odaiq.co>
2021-04-14 21:17:43 +0300

    Created SubscriptionRepository

---
+++ ChromaticCanvas/ChromaticCanvas.ApplicationLogic/Abstractions/ISubscriptionRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChromaticCanvas.ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChromaticCanvas.ApplicationLogic.Abstractions
+{
+    public interface ISubscriptionRepository : IRepository<Subscription>
+    {
+        // TODO: Figure out what to put in here or maybe redefine the subscriptions entity to have a unique identifier.
+    }
+}
