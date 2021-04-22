d9000af9a7c2045fd30278b02d4571dbfb95ce70
Aodai <odai@odaiq.co>
2021-04-22 12:49:33 +0300

    Added a unit test project for ChromaticCanvas.ApplicationLogic

---
+++ ChromaticCanvas/ChromaticCanvas.ApplicationLogic.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChromaticCanvas.ApplicationLogic.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
