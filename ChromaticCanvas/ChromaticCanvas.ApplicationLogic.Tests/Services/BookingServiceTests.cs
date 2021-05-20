3fbfae5eb7fd056af6492aaba74fdd12b71a87ae
Aodai <Aodai@users.noreply.github.com>
2021-05-20 12:51:25 +0300

    Remove unnecessary using statements.

--- ChromaticCanvas/ChromaticCanvas.ApplicationLogic.Tests/Services/BookingServiceTests.cs
+++ ChromaticCanvas/ChromaticCanvas.ApplicationLogic.Tests/Services/BookingServiceTests.cs
@@ -5,10 +5,6 @@ using ChromaticCanvas.ApplicationLogic.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChromaticCanvas.ApplicationLogic.Tests.Services
 {
